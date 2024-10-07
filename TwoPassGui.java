7b12054ab454922160a7673c1330266bc18aca50
bhadraaa <147067215+bhadraaa@users.noreply.github.com>
2024-10-07 19:22:59 +0530

    Update TwoPassGui.java

--- TwoPassGui.java
+++ TwoPassGui.java
@@ -491,3 +491,33 @@ public static String formatTex(String input) {
 }
 
 }
+
+
+/*
+
+COPY	START	1000
+**	LDA 	ALPHA
+**	ADD 	ONE
+**	SUB 	TWO
+**	STA 	BETA
+ALPHA	BYTE	C'CZXE'
+ONE	RESW	2
+TWO	WORD	5
+BETA	RESW	1
+**	END 	-
+
+
+
+
+prog1	START	2000
+**	LDA		FIVE
+**	STA		ALPHA
+** 	LDCH	CHARZ
+** 	STCH	C1
+ALPHA 	RESW 	2
+FIVE 	WORD 	5
+CHARZ 	BYTE 	C'Z'
+C1 	RESB 	1
+** 	END	**
+
+*/
